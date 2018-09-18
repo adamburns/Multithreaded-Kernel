943b2ae380d68efd7ec4b4e4f6c700330d6c0bf0
Adam <me@adamburns.org>
2018-09-18 00:39:57 -0500

    Initialise semaphore, memory file

---
+++ shmsem.c
@@ -0,0 +1,19 @@
+void init_sem(int sem_id, int sem, int init_val)
+{
+
+}
+
+void remove_sem (int sem_id)
+{
+
+}
+
+int attach_shared_mem(int num_bytes, char **addr, int *seg_id)
+{
+
+}
+
+void remove_shared_mem()
+{
+
+}
