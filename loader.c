b105632077e2270022ab12226c6ac26d76ce775c
Adam <me@adamburns.org>
2018-09-18 00:30:55 -0500

    Initialise loader file

---
+++ loader.c
@@ -0,0 +1,30 @@
+/*
+Code to develop a loader, cpu, and printer handler to enable multithreaded operations
+*/
+
+main(int argc, char* argv[])
+{
+
+}
+
+void driver()
+{
+
+}
+
+void loader()
+{
+
+}
+
+void cpu()
+{
+
+}
+
+void printer()
+{
+
+}
+
+
